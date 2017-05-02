040bc97b41ea7ab1831667e67538b94d5c078bca
Muhammad Mohaiminul Islam <IamMohaiminul@gmail.com>
2017-05-02 18:49:31 +0600

    Implement Django Settings for Multiple Environments

---
+++ DjangoRESTfulAPI/settings/local.py
@@ -0,0 +1,20 @@
+from DjangoRESTfulAPI.settings.base import *
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'd7l2-m%ppif%cv-dp32lsqmxtp%wc!yeiw5ks#rw83v&6q%2y3'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../local.sqlite3'),
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DjangoRESTfulAPI
 This is a Boilerplate of a Django Project, which is used for RESTful API service.
+
+```
+$ python manage.py runserver --settings=DjangoRESTfulAPI.settings.local
+```
+```
+$ python manage.py migrate --settings=DjangoRESTfulAPI.settings.local
+```
